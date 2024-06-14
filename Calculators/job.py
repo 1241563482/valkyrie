664bc4c2c78fc302defb340862c002548ef55f06
1241563482@qq.com <1241563482@qq.com>
2024-06-14 15:42:29 +0800

    job.py

---
+++ Calculators/job.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  8 19:48:21 2023
+
+@author: Yijie Zhu
+"""
+
+import sys, os
+sys.path.append('../')
+from set_up import *
+from set_up import __mpirun__
+
+def gen_job(job, job_file):
+    with open(job_file, "r") as file:
+        lines = file.readlines()
+        body = ''.join(lines)
+    with open(job, "w") as file:
+        print(job_head, file = file)
+        print(environment, file = file)
+        print(body, file = file)
+    os.system('sed -i "s/MPIRUN/{}/g" {}'.format(__mpirun__, job))
+    
+        
+def sub(job):
+    os.system(sub_command + job)
+    
+
+def control_job(q, n, comment):
+    with open("job", "r") as file:
+        lines = file.readlines()
+    line_number = 0
+    
+    for line in lines:
+        if comment is not None:
+            if line.startswith(comment_startswith) or line.startswith(comment_startswith.lower()):
+                lines[line_number] = comment_startswith + " " + str(comment) + "\n"
+        if n is not None:
+            if line.startswith(core_number_startswith) or line.startswith(core_number_startswith.lower()):
+                lines[line_number] = core_number_startswith + " " + str(n) + "\n"
+        if q is not None:
+            if line.startswith(node_name_startswith) or line.startswith(node_name_startswith.lower()):
+                lines[line_number] = node_name_startswith + " " + str(q) + "\n"
+        line_number = line_number + 1 
+    
+    with open("job", 'w') as file:
+        file.writelines(lines)
+
+    
+    
+if __name__ == "__main__":
+    gen_job("job", "/home/yijiezhu/valkyrie/shell_scripts/job_relax")
